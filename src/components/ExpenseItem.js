101d8827afab5eb45f3a3830bd8896d7954504e7
mahmoudfat7i <devmahmoudfathy@gmail.com>
2023-05-07 23:39:34 +0300

    2

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (
+        <>
+            <h1>expense Item</h1>
+            <div><Date></Date></div>
+            <div>
+              <h3>car insurance</h3>
+              <div>$255.25</div>
+            </div>
+        </>
+    )
+}
+
+export default ExpenseItem
