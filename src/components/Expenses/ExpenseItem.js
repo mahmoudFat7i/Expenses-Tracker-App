c77c9548577d977f425911d4757c9fe369ff6719
mahmoudfat7i <devmahmoudfathy@gmail.com>
2023-05-22 00:46:04 +0300

    add form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,28 @@
 import './ExpenseItem.css'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
+import { useState } from 'react'
+
 function ExpenseItem(props) {
+    const [amount, addAmount] = useState(props.amount)
+    const [title, setTitle] = useState(props.title)
+    const clickHandler = () => {
+        addAmount(amount + 5)
+        console.log(amount + 6)
+    }
+    const clickHandler1 = () => {
+        setTitle('new title')
+        console.log(title)
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
+                <h2>{title}</h2>
+                <div className="expense-item__price">${amount}</div>
             </div>
+            <button onClick={clickHandler}>Change amount</button>
+            <button onClick={clickHandler1}>Change title</button>
         </Card>
     )
 }
